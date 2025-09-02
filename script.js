d264bb085cb7d5b7d67e8d593310411eec363abf
MieszkoWita <mieszkowita@gmail.com>
2025-09-02 12:43:10 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,18 +1,24 @@
-const posts = [
-  "1post.txt"
-];
-
 async function loadPosts() {
   const container = document.getElementById("posts");
 
-  for (let file of posts) {
+  const res = await fetch("posts.json");
+  const files = await res.json();
+
+  for (let file of files) {
     try {
       const response = await fetch(file);
       const text = await response.text();
 
+      const lines = text.split("\n");
+      const title = lines[0];
+      const content = lines.slice(1).join("\n");
+
       const div = document.createElement("div");
       div.className = "message";
-      div.textContent = text;
+      div.innerHTML = `
+        <h2 class="post-title">${title}</h2>
+        <p>${content}</p>
+      `;
 
       container.appendChild(div);
     } catch (e) {
